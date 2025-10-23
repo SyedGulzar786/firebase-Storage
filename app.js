6c88335089566cd1654aa3a0a42944cd401ca23d
Syed Gulzar Jilani <syedgulzarmohammad123@gmail.com>
2025-10-23 12:33:22 -0700

    file upload firebase, ref, uploadBytesResumable, getDownloadURL, storage
    BEST PRACTICE FOR UPLOADING:
    for example you got 1000 students and you need to store pics of those students but only could accept one from each; save each students profile photo through its CNIC no as image name/reference so if the student uploads another one the previoud gets overwrite by the new one

--- app.js
+++ app.js
@@ -6,7 +6,34 @@ import {
 } from "./firebase.js";
 
 let uploadFile = () => {
-    const flie = document.getElementById("file")
+    const file = document.getElementById("file")
+    console.log(file.files[0])
+    const storageRef = ref(storage, `images/${file.files[0].name}`);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+    uploadTask.on('state_changed',
+        (snapshot) => {
+            const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            console.log('Upload is ' + progress + '% done');
+            switch (snapshot.state) {
+                case 'paused':
+                    console.log('Upload is paused');
+                    break;
+                case 'running':
+                    console.log('Upload is running');
+                    break;
+            }
+        },
+        (error) => {
+            console.error(error)
+        },
+        () => {
+            getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                console.log('File available at', downloadURL);
+            });
+        }
+    );
+
+
 }
 
 const uploadBtn = document.getElementById("uploadBtn");
